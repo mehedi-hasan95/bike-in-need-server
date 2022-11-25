bfbbab52c4290b77dd849a2bebf8ff8f9bba73e7
mehedititu <mh.me.mehedi@gmail.com>
2022-11-25 16:44:18 +0600

    Fiexd try finally error

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,22 +17,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.BIKE_DB_USERNAME}:${process.env.BIKE_DB_PASSWORD}@cluster0.k4gmzpi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const bikeCategory = client.db("bikeInNeed").collection("categories");
-const bikeDetails = client.db("bikeInNeed").collection("details");
+async function run() {
+    try {
+        const bikeCategory = client.db("bikeInNeed").collection("categories");
+        const bikeDetails = client.db("bikeInNeed").collection("details");
 
-app.get('/categories', async (req, res) => {
-    const query = {};
-    const cursor = await bikeCategory.find(query).toArray();
-    res.send(cursor)
-})
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const cursor = await bikeCategory.find(query).toArray();
+            res.send(cursor)
+        })
 
-// Bike Details 
-app.get('/categories/:category', async (req, res) => {
-    const details = req.params.category;
-    const query = {category : details};
-    const cursor = await bikeDetails.find(query).toArray();
-    res.send(cursor)
-})
+        // Bike Details 
+        app.get('/categories/:category', async (req, res) => {
+            const details = req.params.category;
+            const query = { category: details };
+            const cursor = await bikeDetails.find(query).toArray();
+            res.send(cursor)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
 
 
 
